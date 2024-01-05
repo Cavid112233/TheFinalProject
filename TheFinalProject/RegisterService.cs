c2aa0e4547b6b645da9016f53193d684e3a71fb5
Cavid112233 <suleymanli535@gmail.com>
2024-01-06 01:03:07 +0400

    newss

---
+++ TheFinalProject/RegisterService.cs
@@ -0,0 +1,28 @@
+﻿using TheFinalProject.DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TheFinalProject.DAL;
+
+namespace TheFinalProject
+{
+    public static class RegisterService
+    {
+        public static void Register(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            }
+            );
+
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromMinutes(1);
+            });
+
+
+
+        }
+    }
+}
