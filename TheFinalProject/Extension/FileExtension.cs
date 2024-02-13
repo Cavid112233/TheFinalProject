bc7d407ab59292e9d697dff4b10d8c63ea29b77b
Cavid112233 <suleymanli535@gmail.com>
2024-02-13 19:07:27 +0400

    fileextension

---
+++ TheFinalProject/Extension/FileExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace Backend.Extension
+{
+    public static class FileExtension
+    {
+
+        public static bool CheckIMage(this IFormFile file)
+        {
+            return file.ContentType.Contains("image/");
+        }
+        public static bool CheckIMageSize(this IFormFile file, int size)
+        {
+            return file.Length / 1024 > size;
+        }
+
+        public static string SaveImage(this IFormFile file, string folder, IWebHostEnvironment webHostEnvironment)
+        {
+            string fileName = Guid.NewGuid() + file.FileName;
+            string path = Path.Combine(webHostEnvironment.WebRootPath, folder, fileName);
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
